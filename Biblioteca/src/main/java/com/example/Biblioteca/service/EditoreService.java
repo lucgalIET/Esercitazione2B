289807d8195fb5ba28ce6a9400bb446a9e512577
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-03 11:56:21 +0200

    EditoreService

--- Biblioteca/src/main/java/com/example/Biblioteca/service/EditoreService.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/service/EditoreService.java
@@ -1,4 +1,38 @@
 package com.example.Biblioteca.service;
 
+import com.example.Biblioteca.entities.EditoreEntity;
+import com.example.Biblioteca.repository.EditoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EditoreService {
-}
+    @Autowired
+    private EditoreRepository editoreRepository;
+
+
+    public EditoreEntity saveEditore(EditoreEntity editore) {
+        return editoreRepository.save(editore);
+    }
+
+    public List<EditoreEntity> getAllEditori() {
+        return editoreRepository.findAll();
+    }
+
+    public Optional<EditoreEntity> getEditoreById(Long id) {
+        return editoreRepository.findById(id);
+    }
+
+    public EditoreEntity updateEditore(EditoreEntity editore) {
+
+        Optional<EditoreEntity> editoreEntity = getEditoreById(editore.getIdEditore());
+        if (editoreEntity.isEmpty()) throw new NullPointerException();
+
+        return editoreRepository.save(editore);
+    }
+    public void deleteProdotto(Long id) {
+        editoreRepository.deleteById(id);
+    }}
