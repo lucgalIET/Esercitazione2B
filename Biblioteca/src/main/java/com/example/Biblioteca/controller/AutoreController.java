03360ca6daf171dca5d2a83fca7d4e33c66642f6
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-03 12:08:58 +0200

    AutoreController

--- Biblioteca/src/main/java/com/example/Biblioteca/controller/AutoreController.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/controller/AutoreController.java
@@ -1,4 +1,35 @@
 package com.example.Biblioteca.controller;
 
+import com.example.Biblioteca.entities.AutoreEntity;
+import com.example.Biblioteca.service.AutoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/autore")
 public class AutoreController {
+    @Autowired
+    private AutoreService autoreService;
+    @GetMapping("/autore/get-all")
+    public List<AutoreEntity> getAllAutore(){
+        return autoreService.getAllAutore();
+    }
+    @GetMapping("/get-autore/{id}")
+    public Optional<AutoreEntity> getAutoreById(@PathVariable Long id){
+        return autoreService.getAutoreById(id);
+    }
+
+    @PostMapping("/salva-autore")
+    public AutoreEntity saveAutore(@RequestBody AutoreEntity autore) {return autoreService.saveAutore(autore);}
+
+    @PostMapping("/autore/modifica-autore/{id}")
+    public AutoreEntity updateAutore(@RequestBody AutoreEntity autore) {return autoreService.saveAutore(autore);}
+
+    @PostMapping("/autore/delete-autore/{id}")
+    public void deleteAutore(@RequestBody Long id) { autoreService.deleteAutore(id);}
+
+
 }
