b04ffbbc11384c68eb85ea67b6a4511b4319be4d
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-03 13:16:29 +0200

    commit

--- Biblioteca/src/main/java/com/example/Biblioteca/repository/LibroRepository.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/repository/LibroRepository.java
@@ -2,8 +2,18 @@ package com.example.Biblioteca.repository;
 
 import com.example.Biblioteca.entities.LibroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface LibroRepository extends JpaRepository<LibroEntity,Long> {
-}
 
+    @Query(value = "SELECT l.titolo,l.pagine,l.prezzo" +
+            "FROM libro l" +
+            "WHERE pagine>400", nativeQuery = true)
+    List<LibroEntity> libriConPiuDi400Pagine();
+
+
+}
