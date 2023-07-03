b6bbc0c317a871dd7df03e3b08f11ec98e24843c
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-03 12:23:54 +0200

    EditoreController

--- Biblioteca/src/main/java/com/example/Biblioteca/controller/EditoreController.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/controller/EditoreController.java
@@ -10,26 +10,26 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/autore")
+@RequestMapping("/api/editore")
 public class EditoreController {
     @Autowired
     private EditoreService editoreService;
-    @GetMapping("/autore/get-all")
+    @GetMapping("/get-all")
     public List<EditoreEntity> getAllEditore(){
         return editoreService.getAllEditore();
     }
-    @GetMapping("/get-autore/{id}")
+    @GetMapping("/get-editore/{id}")
     public Optional<EditoreEntity> getEditoreById(@PathVariable Long id){
         return editoreService.getEditoreById(id);
     }
 
-    @PostMapping("/salva-autore")
+    @PostMapping("/salva-editore")
     public EditoreEntity saveEditore(@RequestBody EditoreEntity editore) {return editoreService.saveEditore(editore);}
 
     @PostMapping("/modifica-editore/{id}")
     public EditoreEntity updateEditore(@RequestBody EditoreEntity editore) {return editoreService.saveEditore(editore);}
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteEditore(Long id){
         editoreService.deleteEditore(id);
     }
