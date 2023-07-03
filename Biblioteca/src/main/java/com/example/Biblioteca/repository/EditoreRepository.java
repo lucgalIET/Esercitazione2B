c07586c2c42091fa71a90fcb5f68321387ea9aab
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-03 13:06:21 +0200

    Custom Query

--- Biblioteca/src/main/java/com/example/Biblioteca/repository/EditoreRepository.java
+++ Biblioteca/src/main/java/com/example/Biblioteca/repository/EditoreRepository.java
@@ -2,9 +2,16 @@ package com.example.Biblioteca.repository;
 
 import com.example.Biblioteca.entities.EditoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Repository
 public interface EditoreRepository  extends JpaRepository<EditoreEntity,Long> {
+
+    @Query(value = "SELECT nome, contatto FROM editore", nativeQuery = true )
+    List<String> getStudenteNomeContatto();
 
 }
