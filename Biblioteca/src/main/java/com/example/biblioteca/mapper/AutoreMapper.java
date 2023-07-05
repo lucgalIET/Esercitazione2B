fbab39bcb141989dc51c6bbcc21570d062aee7f7
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-05 09:31:32 +0200

    commit

--- Biblioteca/src/main/java/com/example/biblioteca/mapper/AutoreMapper.java
+++ Biblioteca/src/main/java/com/example/biblioteca/mapper/AutoreMapper.java
@@ -8,15 +8,15 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AutoreMapper {
-    AutoreMapper AUTORE_MAPPER= Mappers.getMapper(AutoreMapper.class);
+    AutoreMapper AUTORE_MAPPER = Mappers.getMapper(AutoreMapper.class);
 
-    @Mapping(source = "idAutore",target = "idAutore")
-    @Mapping(source = "nomeAutore",target = "nomeAutore")
-    @Mapping(source = "nazionalitaAutore",target = "nazionalitaAutore")
+    @Mapping(source = "idAutore", target = "idAutore")
+    @Mapping(source = "nomeAutore", target = "nomeAutore")
+    @Mapping(source = "nazionalitaAutore", target = "nazionalitaAutore")
     AutoreDTO entityToDto(AutoreEntity autoreEntity);
 
-    @Mapping(source = "idAutore",target = "idAutore")
-    @Mapping(source = "nomeAutore",target = "nomeAutore")
-    @Mapping(source = "nazionalitaAutore",target = "nazionalitaAutore")
-    AutoreEntity dtoToEntity (AutoreDTO autoreDTO);
+    @Mapping(source = "idAutore", target = "idAutore")
+    @Mapping(source = "nomeAutore", target = "nomeAutore")
+    @Mapping(source = "nazionalitaAutore", target = "nazionalitaAutore")
+    AutoreEntity dtoToEntity(AutoreDTO autoreDTO);
 }
---
+++ Biblioteca/src/main/java/com/example/biblioteca/mapper/EditoreMapper.java
@@ -0,0 +1,27 @@
+package com.example.biblioteca.mapper;
+
+import com.example.biblioteca.dto.AutoreDTO;
+import com.example.biblioteca.dto.EditoreDTO;
+import com.example.biblioteca.entities.AutoreEntity;
+import com.example.biblioteca.entities.EditoreEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface EditoreMapper {
+    EditoreMapper EDITORE_MAPPER = Mappers.getMapper(EditoreMapper.class);
+
+    @Mapping(source = "idEditore", target = "idEditore")
+    @Mapping(source = "nomeEditore", target = "nomeEditore")
+    @Mapping(source = "contattoEditore", target = "contattoEditore")
+    EditoreDTO entityToDto(EditoreEntity editoreEntity);
+
+    @Mapping(source = "idEditore", target = "idEditore")
+    @Mapping(source = "nomeEditore", target = "nomeEditore")
+    @Mapping(source = "nazionalitaAutore", target = "nazionalitaAutore")
+    EditoreEntity dtoToEntity(EditoreDTO editoreDTO);
+
+
+
+}
