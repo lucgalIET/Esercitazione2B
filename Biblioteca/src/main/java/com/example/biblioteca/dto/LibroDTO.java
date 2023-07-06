6f2ac25c1a3790f5f66d6cd848a086d368f55075
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-06 13:26:21 +0200

    commit

--- Biblioteca/src/main/java/com/example/biblioteca/dto/LibroDTO.java
+++ Biblioteca/src/main/java/com/example/biblioteca/dto/LibroDTO.java
@@ -5,6 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+fare tutte le crud con i dto
+1 libro deve avere almeno 1 autore e
+implementare tutto nel service e non nel controller(solo endpoint)
+ */
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -14,4 +23,5 @@ public class LibroDTO {
     private String titolo;
     private int pagine;
     private double prezzo;
+    private List<AutoreDTO> autoreDTOList = new ArrayList<>();
 }
