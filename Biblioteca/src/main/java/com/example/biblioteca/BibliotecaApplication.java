40d97fecbc8b10c9f1d128be6b698514c9b11222
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-06 13:26:44 +0200

    commit

--- Biblioteca/src/main/java/com/example/biblioteca/BibliotecaApplication.java
+++ Biblioteca/src/main/java/com/example/biblioteca/BibliotecaApplication.java
@@ -3,6 +3,12 @@ package com.example.biblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+fare tutte le crud con i dto
+1 libro deve avere almeno 1 autore e
+implementare tutto nel service e non nel controller(solo endpoint)
+ */
+
 @SpringBootApplication
 public class BibliotecaApplication {
 
