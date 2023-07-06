5ca3f36ac69e0655f93df25fcef7d63b8b8d1fb3
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-06 13:13:19 +0200

    fix import

--- Biblioteca/src/main/java/com/example/biblioteca/service/AutoreService.java
+++ Biblioteca/src/main/java/com/example/biblioteca/service/AutoreService.java
@@ -1,15 +1,10 @@
 package com.example.biblioteca.service;
-
 import com.example.biblioteca.dto.AutoreDTO;
-import com.example.biblioteca.dto.LibroDTO;
 import com.example.biblioteca.entities.AutoreEntity;
-import com.example.biblioteca.entities.LibroEntity;
 import com.example.biblioteca.mapper.AutoreMapper;
-import com.example.biblioteca.mapper.LibroMapper;
 import com.example.biblioteca.repository.AutoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -45,14 +40,14 @@ public class AutoreService {
     }
 
 
-
     public List<AutoreDTO> getAutoreInfo() {
         List<AutoreEntity> autoreEntityList = autoreRepository.getAutoreInfo();
         List<AutoreDTO> autoreDTOList = new ArrayList<>();
         for (int i = 0; i < autoreEntityList.size(); i++) {
             autoreDTOList.add(AutoreMapper.AUTORE_MAPPER.entityToDto(autoreEntityList.get(i)));
         }
-        return autoreDTOList;}
+        return autoreDTOList;
+    }
 
 
 }
