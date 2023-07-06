6efdce42208b1205dd76ba89f2cfb58cf1f34ca8
mak-1 <38510654+mak-1@users.noreply.github.com>
2023-07-06 13:11:39 +0200

    autore DTO

--- Biblioteca/src/main/java/com/example/biblioteca/service/AutoreService.java
+++ Biblioteca/src/main/java/com/example/biblioteca/service/AutoreService.java
@@ -1,11 +1,16 @@
 package com.example.biblioteca.service;
 
 import com.example.biblioteca.dto.AutoreDTO;
+import com.example.biblioteca.dto.LibroDTO;
 import com.example.biblioteca.entities.AutoreEntity;
+import com.example.biblioteca.entities.LibroEntity;
+import com.example.biblioteca.mapper.AutoreMapper;
+import com.example.biblioteca.mapper.LibroMapper;
 import com.example.biblioteca.repository.AutoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,9 +44,15 @@ public class AutoreService {
         autoreRepository.deleteById(id);
     }
 
-    public List<AutoreEntity> getAutoreInfo(){return autoreRepository.getAutoreInfo();}
 
 
+    public List<AutoreDTO> getAutoreInfo() {
+        List<AutoreEntity> autoreEntityList = autoreRepository.getAutoreInfo();
+        List<AutoreDTO> autoreDTOList = new ArrayList<>();
+        for (int i = 0; i < autoreEntityList.size(); i++) {
+            autoreDTOList.add(AutoreMapper.AUTORE_MAPPER.entityToDto(autoreEntityList.get(i)));
+        }
+        return autoreDTOList;}
 
 
 }
